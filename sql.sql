2015ed37b5ccdda459c85bd87a124b6e79887a95
vivek sharma <133657968+0408vivek@users.noreply.github.com>
2023-08-21 11:26:57 +0530

    Add files via upload

---
+++ sql.sql
@@ -0,0 +1,18 @@
+show databases;
+
+create database sale;
+
+create table customer_info(
+id int,first_name varchar(25),last_name varchar(25)
+);
+
+use sale;
+show tables;
+
+## insert somerows inside customer_info table
+insert into customer_info values(1,"krish","naik");
+select * from customer_info;
+show tables;
+insert into customer_info values(1,"krish","naik"
+use poonam;
+show tables;
